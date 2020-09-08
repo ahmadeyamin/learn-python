dd56d56e67582ec94143bd0ba5a2d6e082f59b90
ahmadeyamin <ahmadeyamin@gmail.com>
2020-09-08 12:00:21 +0000

    Banglalink Auto Login

--- python.py
+++ python.py
@@ -1,26 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44',
+    'Content-type': 'application/x-www-form-urlencoded',
+    'X-Requested-With': 'XMLHttpRequest'
 }
-data = {
+config = {
     '_method': 'POST',
-    'mobile': '01999050360',
-    'password': '9988776655.C0M'
+    '_csrfToken': None
 }
-with requests.Session() as s:
-    web = s.get('https://eselfcare.banglalink.net/home/index',headers=headers)
 
-    ss = BeautifulSoup(web.content,'html.parser')
-
-    data['_csrfToken'] = ss.find('input',attrs={'name':'_csrfToken'})['value']
-
-    login = s.post('https://eselfcare.banglalink.net/home/index',headers=headers,data=data)
-
-    next_d = s.get('https://eselfcare.banglalink.net/ecare',headers=headers)
+all_accounts = [
+    {
+        'mobile': '*********',
+        'password': '***********',
+    },
+    {
+        'mobile': '**********',
+        'password': '***********',
+    },
+    {
+        'mobile': '**********',
+        'password': '***********',
+    },
     
-    # print(next_d.text)
 
+]
 
-print(login.text)
+while True:
+
+    for info in all_accounts:
+        
+        with requests.Session() as session:
+
+            web = session.get('https://eselfcare.banglalink.net/?redirect=%2Fecare', headers=headers)
+            ss = BeautifulSoup(web.content, 'html.parser')
+
+            config['mobile'] = info['mobile']
+            config['password'] = info['password']
+            config['_csrfToken'] = ss.find('input',attrs={'name':'_csrfToken'})['value']
+
+            login = session.post('https://eselfcare.banglalink.net/home/index',headers=headers,data=config)
+            next_d = session.get('https://eselfcare.banglalink.net/ecare',headers=headers)
+
+            check = BeautifulSoup(next_d.text,'html.parser')
+            if (check.select(".author-mobile h4")[0].get_text() == info['mobile']):
+                print('Login Successfull')
+            else:
+                print('Login Failed')
+
+    time.sleep((60*1)*5)
+    print('End Sleep')
